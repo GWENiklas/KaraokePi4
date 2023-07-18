c96bbc136acca3270de3b8a43545dcbb6941f09c
GWENiklas <139154116+GWENiklas@users.noreply.github.com>
2023-07-18 09:48:44 +0200

    Update routes.js

--- SimpleAPI/routes/routes.js
+++ SimpleAPI/routes/routes.js
@@ -2,8 +2,6 @@ var songsController = require("./songs/songs.js");
 var playersController = require("./players/players.js");
 var wishlistController = require("./wishlist/wishlist.js");
 var settingsController  = require('./settings/settings.js');
-#var shutdownController = require('./shutdown/shutdown.js');
-# Shutdown per URL deaktiviert, solange keine Sicherheitsmechanismen aktiv.
 var uploadController = require('./upload/upload.js')
 var appRouter = function(app) {
     songsController(app);
@@ -15,3 +13,6 @@ var appRouter = function(app) {
 };
 
 module.exports = appRouter;
+
+# Shutdown per URL deaktiviert, solange keine Sicherheitsmechanismen aktiv.
+# var shutdownController = require('./shutdown/shutdown.js');
