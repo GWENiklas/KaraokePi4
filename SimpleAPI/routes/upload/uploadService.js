ab203a2d23ba96eb8bff7926c8058e70f9d4cb43
jort93 <jort93@gmx.de>
2023-08-02 21:37:58 +0200

    Fixed upload service. Now using ffprobe

--- SimpleAPI/routes/upload/uploadService.js
+++ SimpleAPI/routes/upload/uploadService.js
@@ -1,3 +1,5 @@
+var spawnSync = require('child_process').spawnSync;
+
 let uploadPath = "/media/Kara/manueller_upload/" // ./ current directory ../ directory one level up
 
 function getFileExtension(filename) {
@@ -8,8 +10,6 @@ exports.upload = function(req, res, next) {
     var fstream;
     req.pipe(req.busboy);
     req.busboy.on('file', function (fieldname, file, filename) {
-        //console.log("Uploading: " + filename);
-        //console.log(getFileExtension(filename));
         if (filename != '') {
             if (getFileExtension(filename) == "mp4" || getFileExtension(filename) == "avi"|| getFileExtension(filename) == "mkv") {
                 
@@ -19,22 +19,23 @@ exports.upload = function(req, res, next) {
                     if (rows.length == 0) {
                         //Path where video will be uploaded
                         fstream = fs.createWriteStream(uploadPath + filename);
-
                         file.pipe(fstream);
-                        fstream.on('close', function () { 
-                            
-                            new ffmpeg(uploadPath + filename, function (err, video) {
-                                console.log(video);
-                                if (!err) {
-                                var duration = video.metadata.duration.raw
-                                    mysql.connection.query('INSERT INTO conka.songs (pfad, datei, active, duration) VALUES (?, ?, 1, ?)', [uploadPath, filename, duration], function(err, rows, fields) {
-                                        //console.log("Upload Finished of " + filename);           
-                                        res.redirect('/#/admin');           //where to go next
-                                    })     
-                                    }
-                        
+                        fstream.on('close', function () {
+                        	var info; 
+							try {
+								var out = spawnSync("ffprobe", [ "-hide_banner",  "'" + uploadPath + filename + "'" ], { shell: true }).output.toString();
+								info = out;
+							} catch (e) {
+								console.log("Konnte Zeit nicht auslesen, default 3 Minuten.");
+								info = "Duration: 00:03:00.00";
+								console.log(e)
+							}
+							var duration = info.substring(info.indexOf("Duration")+10,info.indexOf("Duration")+21);
+							mysql.connection.query('INSERT INTO conka.songs (pfad, datei, active, duration) VALUES (?, ?, 1, ?)', [uploadPath, filename, duration], function(err, rows, fields) {
+								console.log("Upload Finished of " + filename);           
+								res.redirect('/#/admin');           //where to go next
+								})	
                             });
-                        });
                     } else {
                         res.redirect('/#/admin')
                     }
